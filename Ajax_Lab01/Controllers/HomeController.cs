63cb6994e4e7d8b240c3514f909ae9ddd1225afd
jackey <a30786531@gmail.com>
2024-08-09 16:57:19 +0800

    註冊會員

--- Ajax_Lab01/Controllers/HomeController.cs
+++ Ajax_Lab01/Controllers/HomeController.cs
@@ -10,11 +10,12 @@ namespace Ajax_Lab01.Controllers {
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
         private MyDbContext _dbContext;
-
-        public HomeController(ILogger<HomeController> logger,MyDbContext dbContext)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public HomeController(ILogger<HomeController> logger,MyDbContext dbContext,IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
             _dbContext = dbContext;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -85,7 +86,30 @@ namespace Ajax_Lab01.Controllers {
         }
         public IActionResult Register(userInfo p)
         {
-            return Content($"{p.userName} - {p.userPwd} - {p.userEmail}");
+            string info = $"{p.userPhoto.Length} {p.userPhoto.FileName}";
+            string strPath = $"{_webHostEnvironment.WebRootPath}/images/{p.userPhoto.FileName}";
+
+            using(var fileStream = new FileStream(strPath,FileMode.Create)) {
+                p.userPhoto.CopyTo(fileStream);
+            }
+            Byte[]? img = null;
+            using(var memoryStream = new MemoryStream()) {
+                p.userPhoto.CopyTo(memoryStream);
+                img = memoryStream.ToArray();
+            }
+
+            Member member = new Member {
+                Name = p.userName,
+                //Age = p.userAge,
+                Email = p.userEmail,
+                Password = p.userPwd,
+                FileName = p.userPhoto.FileName,
+                FileData = img
+            };
+
+            _dbContext.Members.Add(member);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
         [ResponseCache(Duration = 0,Location = ResponseCacheLocation.None,NoStore = true)]
         public IActionResult Error()
@@ -104,6 +128,10 @@ namespace Ajax_Lab01.Controllers {
             set;
         }
         public string userEmail {
+            get;
+            set;
+        }
+        public IFormFile? userPhoto {
             get;
             set;
         }

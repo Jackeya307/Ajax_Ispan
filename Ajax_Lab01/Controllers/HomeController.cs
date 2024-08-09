54d8a2d1fe06a785b29df39b4fb46804cc640608
jackey <a30786531@gmail.com>
2024-08-09 15:17:15 +0800

    formData

--- Ajax_Lab01/Controllers/HomeController.cs
+++ Ajax_Lab01/Controllers/HomeController.cs
@@ -79,10 +79,33 @@ namespace Ajax_Lab01.Controllers {
             return View();
         }
 
+        public IActionResult IspanLoginPratice()
+        {
+            return View();
+        }
+        public IActionResult Register(userInfo p)
+        {
+            return Content($"{p.userName} - {p.userPwd} - {p.userEmail}");
+        }
         [ResponseCache(Duration = 0,Location = ResponseCacheLocation.None,NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class userInfo {
+        public string userName {
+            get;
+            set;
+        }
+        public string userPwd {
+            get;
+            set;
+        }
+        public string userEmail {
+            get;
+            set;
         }
     }
 }
